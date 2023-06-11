d1954a4d76ef956d661fb3895855e6e58f8da15b
m-calabresi <mattia.calabresi@gmail.com>
2023-06-11 11:54:00 +0200

    TEST: test env variables

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -12,5 +12,7 @@ export function classNames(...classes) {
  * @returns
  */
 export function path(path) {
+    alert(import.meta.env.PUBLIC_BASE_PATH);
+    alert(`${import.meta.env.PUBLIC_BASE_PATH ?? ""}${path}`);
     return `${import.meta.env.PUBLIC_BASE_PATH ?? ""}${path}`;
 }
