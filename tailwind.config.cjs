e6f5c9d20dffca90abf6fcddc56775cb3b1cb44c
m-calabresi <mattia.calabresi@gmail.com>
2023-06-11 10:51:12 +0200

    ENH: added LoadScreen component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,34 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                fade: {
+                    "0%": {
+                        opacity: 1,
+                    },
+                    "100%": {
+                        opacity: 0,
+                    },
+                },
+            },
+            animation: {
+                fade: "fade 0.4s ease 0.4s 1 normal forwards running",
+            },
+            fontFamily: {
+                sans: ["Montserrat Variable", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-    plugins: [],
+    plugins: [
+        plugin(function ({ addVariant }) {
+            addVariant("sticking", "&.sticking");
+            addVariant('group-sticking', ':merge(.group).sticking &');
+            addVariant('peer-sticking', ':merge(.peer).sticking ~ &');
+        }),
+    ],
 };
