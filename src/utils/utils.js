a86fd7c5c4a4a177d4050a10dae154485208c8bc
m-calabresi <mattia.calabresi@gmail.com>
2023-06-11 12:37:25 +0200

    BUG: fixed path construction issues 2

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -7,10 +7,9 @@ export function classNames(...classes) {
 }
 
 /**
- *
  * @param {string} path
  * @returns {string}
  */
 export function path(path) {
-    return `${import.meta.env.PUBLIC_BASE_PATH ?? ""}${path}`.split("//").join("/");
+    return `${import.meta.env.PUBLIC_BASE_PATH ?? "/Essedue"}${path}`.split("//").join("/");
 }
