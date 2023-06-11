49994bdeff6a2cc228249e4a131eb2b1d9c12ce9
m-calabresi <mattia.calabresi@gmail.com>
2023-06-11 11:31:10 +0200

    MAINT: added URL env variables for production and development stages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
 
 import tailwind from "@astrojs/tailwind";
+
+const { PUBLIC_BASE_PATH, PUBLIC_SITE } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
-    site: "https://m-calabresi.github.io",
-    base: "/Essedue",
+    site: PUBLIC_SITE,
+    base: PUBLIC_BASE_PATH,
 });
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,16 @@
 /**
- * @param {string[]} classes 
+ * @param {string[]} classes
  * @returns {string}
  */
 export function classNames(...classes) {
-    return classes.filter(Boolean).join(" ")
+    return classes.filter(Boolean).join(" ");
+}
+
+/**
+ *
+ * @param {string} path
+ * @returns
+ */
+export function path(path) {
+    return `${import.meta.env.PUBLIC_BASE_PATH}${path}`;
 }
