a0038f7524082e8975da73ea9fba31972ec5040a
m-calabresi <mattia.calabresi@gmail.com>
2023-06-25 12:03:58 +0200

    ENH: converted img to Astro Image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
-
 import tailwind from "@astrojs/tailwind";
-
+import image from "@astrojs/image";
 const { PUBLIC_BASE_PATH, PUBLIC_SITE } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [tailwind(), image({ serviceEntryPoint: "@astrojs/image/sharp" })],
     site: PUBLIC_SITE,
     base: PUBLIC_BASE_PATH,
 });
