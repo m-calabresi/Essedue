4133e0e52e9fc15657050275426e92eca2284032
m-calabresi <mattia.calabresi@gmail.com>
2023-08-14 11:59:21 +0200

    MAINT: moved to Astro experimental Image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 const { PUBLIC_BASE_PATH, PUBLIC_SITE } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 
 // https://astro.build/config
@@ -9,4 +8,7 @@ export default defineConfig({
     integrations: [tailwind(), image({ serviceEntryPoint: "@astrojs/image/sharp" })],
     site: PUBLIC_SITE,
     base: PUBLIC_BASE_PATH,
+    experimental: {
+        assets: true,
+    },
 });
