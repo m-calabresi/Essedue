884df2fa195e3a2d8f7073c46fa69a06da0aa149
m-calabresi <mattia.calabresi@gmail.com>
2023-08-18 12:51:04 +0200

    MAINT: added proper support for components class prop

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,3 @@
-export function classNames(...classes: string[]): string {
-    return classes.filter(Boolean).join(" ");
-}
-
 export function path(path: string): string {
     return `${import.meta.env.PUBLIC_BASE_PATH ?? "/Essedue"}${path}`.split("//").join("/");
 }
