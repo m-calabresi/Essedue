3e172230682e390fb860c369c41e4e97a842c3de
m-calabresi <mattia.calabresi@gmail.com>
2024-01-10 20:47:43 +0100

    ENH: added sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 const { PUBLIC_BASE_PATH, PUBLIC_SITE } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 
@@ -16,6 +17,14 @@ export default defineConfig({
             applyBaseStyles: false,
         }),
         react(),
+        sitemap({
+            i18n: {
+                defaultLocale: "it",
+                locales: {
+                    it: "it",
+                },
+            },
+        }),
     ],
     site: PUBLIC_SITE,
     base: PUBLIC_BASE_PATH,
