8173863fa0550a292ae8070c149ebb8dd70de704
Mattia Calabresi <mattia.calabresi@gmail.com>
2025-06-14 18:54:42 +0200

    BLD: add version check to git tag

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,5 +2,11 @@
 
 GIT_VERSION=""
 
+# Check if version follows 'vX.Y.Z' or 'vX.Y.Z-suffix123' format
+if [[ ! $GIT_VERSION =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z0-9]+)?$ ]]; then
+    echo "Error: Version '$GIT_VERSION' doesn't follow formats 'v1.2.3' or 'v1.2.3-suffix123'"
+    exit 1
+fi
+
 git tag ${GIT_VERSION}
 git push origin ${GIT_VERSION}
